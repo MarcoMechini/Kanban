af7f13a19d8ef0c48805662ba6b9010d8ca8db5f
Marco Mechini <mechinimarco35@gmail.com>
2025-05-21 13:11:12 +0200

    Refactor AppModal to handle input changes and confirm updates; enhance task management in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faEllipsis } from '@fortawesome/free-solid-svg-icons'
 
-const AppModal = ({ isOpen, setColumn }) => {
+const AppModal = ({ isOpen, value, setModal, onConfirm }) => {
   if (!isOpen) return null
-  // console.log(value);
-  console.log('dentol il moda');
+  console.log(value);
 
+  const handleChange = (e) => {
+    setModal(prev => ({ isOpen: prev.isOpen, value: { id: value.id, name: e.target.value, tasks: value.tasks } }))
+    console.log('value', value);
+
+  }
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        {/* <input type="text" value={value.name} onChange={setColumn(prev => prev.find(curCol.value.id === col))} />
-        {value.tasks.map((task, index) => (
-          <div key={index} className="task">
-            <input type="text" value={task.name} />
-            <button className='delete-task'>
-              <FontAwesomeIcon icon={faEllipsis} />
-            </button>
-          </div>
-        ))} */}
+        <label htmlFor="name">Nome</label>
+        <input type="text"
+          value={value.name}
+          onChange={handleChange} />
+        <button onClick={() => setModal(prev => ({ isOpen: !prev.isOpen, value: prev.value }))}>
+          Chiudi</button>
+        <button onClick={onConfirm}>Conferma</button>
       </div>
     </div>
   )
@@ -27,22 +29,42 @@ const AppModal = ({ isOpen, setColumn }) => {
 
 function App() {
   const [column, setColumn] = useState([])
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState({ isOpen: false, value: {} })
 
   const addColumn = () => {
     const newColumn = {
       id: Date.now(),
       name: 'New Column',
-      tasks: []
+      tasks: [{ title: 'Task', desc: 'Descrizione' }]
     }
     setColumn(prev => [...prev, newColumn])
-    console.log(column);
   }
 
-  const handleModal = () => {
-    console.log('sono qui');
+  const addTask = (_, col) => {
+    console.log(col);
+    setColumn(prev => prev.map(c => {
+      if (c.id === col.id) {
+        return { ...c, tasks: [...c.tasks, { title: 'Task', desc: 'Descrizione' }] }
+      }
+      return c
+    }))
+  }
 
-    setModal(prev => (!prev))
+  const handleConfirm = () => {
+    setColumn(prev => prev.map(col => {
+      if (col.id === modal.value.id) {
+        return { ...col, name: modal.value.name }
+      }
+      return col
+    }))
+    setModal(prev => ({ isOpen: !prev.isOpen, value: prev.value }))
+  }
+
+  const handleModal = (col) => {
+    setModal(prev => ({
+      isOpen: !prev.isOpen,
+      value: col
+    }))
   }
 
   return (
@@ -59,14 +81,25 @@ function App() {
           <div key={index}>
             <div className='col-header'>
               <h3>{col.name}</h3>
-              <button className='edit-name' onClick={handleModal}>
+              <button className='edit-name' onClick={() => handleModal(col)}>
                 <FontAwesomeIcon icon={faEllipsis} />
               </button>
             </div>
+            <div className='col-body'>
+              <button onClick={e => addTask(e, col)}>+</button>
+              {col.tasks.map((task, index) => {
+                return (
+                  <div key={index} className='tasks-row'>
+                    <h4>{task.title}</h4>
+                    <p>{task.desc}</p>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         )}
-      </section>
-      <AppModal isOpen={modal} setColumn={setColumn} />
+      </section >
+      <AppModal isOpen={modal.isOpen} value={modal.value} onConfirm={handleConfirm} setModal={setModal} setColumn={setColumn} />
     </>
   )
 }
